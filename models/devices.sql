beb3184b1acef352f6b7b25746ffc7af26967373
Trevor Hutto <trevorhutto@fullstory.com>
2024-03-08 11:14:44 -0500

    fix negative space

--- models/devices.sql
+++ models/devices.sql
@@ -5,7 +5,7 @@ select
   updated_time,
   processed_time,
   device_user_agent as user_agent,
-  device_type as {%- if target.type == "redshift" -%}"type"{%- else -%}type{%- endif -%},
+  device_type as {% if target.type == "redshift" -%}"type"{%- else -%}type{%- endif -%},
   device_operating_system as operating_system,
   device_browser as browser,
   device_browser_version as browser_version,
