2ca3fdccb64a63f6a7541e2774d33e05284b6091
Robele Baker <robele@fullstory.com>
2025-03-12 08:51:01 -0700

    fix integration test session_device_identify_user_works and casted shim models in integration tests

--- integration_tests/models/redshift_events_shim.sql
+++ integration_tests/models/redshift_events_shim.sql
@@ -1,13 +1,13 @@
 select
-  event_id,
-  event_time,
-  processed_time,
-  updated_time,
-  device_id,
-  session_id,
-  view_id,
-  event_type,
+  {{ dbt.cast("event_id", api.Column.translate_type("string")) }} as event_id,
+  {{ dbt.cast("device_id", api.Column.translate_type("string")) }} as device_id,
+  {{ dbt.cast("session_id", api.Column.translate_type("string")) }} as session_id,
+  {{ dbt.cast("view_id", api.Column.translate_type("string")) }} as view_id,
+  {{ dbt.cast("event_time", api.Column.translate_type("datetime")) }} as event_time,
+  {{ dbt.cast("event_type", api.Column.translate_type("string")) }} as event_type,
+  {{ dbt.cast("source_type", api.Column.translate_type("string")) }} as source_type,
+  {{ dbt.cast("updated_time", api.Column.translate_type("datetime")) }} as updated_time,
+  {{ dbt.cast("processed_time", api.Column.translate_type("datetime")) }} as processed_time,
   JSON_PARSE(event_properties::varchar) as event_properties,
-  source_type,
   JSON_PARSE(source_properties::varchar) as source_properties
 from {{ ref('fullstory_events_integration_seeds') }}
--- integration_tests/models/snowflake_events_shim.sql
+++ integration_tests/models/snowflake_events_shim.sql
@@ -1,13 +1,13 @@
 select
-  event_id,
-  event_time,
-  processed_time,
-  updated_time,
-  device_id,
-  session_id,
-  view_id,
-  event_type,
+  {{ dbt.cast("event_id", api.Column.translate_type("string")) }} as event_id,
+  {{ dbt.cast("device_id", api.Column.translate_type("string")) }} as device_id,
+  {{ dbt.cast("session_id", api.Column.translate_type("string")) }} as session_id,
+  {{ dbt.cast("view_id", api.Column.translate_type("string")) }} as view_id,
+  {{ dbt.cast("event_time", api.Column.translate_type("datetime")) }} as event_time,
+  {{ dbt.cast("event_type", api.Column.translate_type("string")) }} as event_type,
+  {{ dbt.cast("source_type", api.Column.translate_type("string")) }} as source_type,
+  {{ dbt.cast("updated_time", api.Column.translate_type("datetime")) }} as updated_time,
+  {{ dbt.cast("processed_time", api.Column.translate_type("datetime")) }} as processed_time,
   parse_json(event_properties) as event_properties,
-  source_type,
   parse_json(source_properties) as source_properties
 from {{ ref('fullstory_events_integration_seeds') }}
