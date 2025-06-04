71faa2295784111dcac020cde471f2798bcd8845
Robele Baker <robele@fullstory.com>
2025-06-04 05:00:32 -0700

    remove duplicate events and fixed intermediate identifies model

--- models/anonymous_users.sql
+++ models/anonymous_users.sql
@@ -6,34 +6,33 @@
 
 with devices as (
 
-  select * from {{ ref('devices') }}
-)
+    select * from {{ ref('devices') }}
+),
 
-, identifies as (
+identifies as (
 
-  select * from {{ ref('identifies') }}
+    select * from {{ ref('identifies') }}
 )
 
 select
-  devices.id as device_id,
-  devices.user_agent as last_device_user_agent,
-  devices.type as last_device_type,
-  devices.operating_system as last_device_operating_system,
-  devices.browser as last_device_browser,
-  devices.browser_version as last_device_browser_version,
-  devices.geo_ip_address as last_geo_ip_address,
-  devices.geo_country as last_geo_country,
-  devices.geo_region as last_geo_region,
-  devices.geo_city as last_geo_city,
-  devices.geo_lat_long as last_geo_lat_long,
-  devices.event_time as last_event_time,
-  devices.event_id as last_event_id
+    devices.id as device_id,
+    devices.user_agent as last_device_user_agent,
+    devices.type as last_device_type,
+    devices.operating_system as last_device_operating_system,
+    devices.browser as last_device_browser,
+    devices.browser_version as last_device_browser_version,
+    devices.geo_ip_address as last_geo_ip_address,
+    devices.geo_country as last_geo_country,
+    devices.geo_region as last_geo_region,
+    devices.geo_city as last_geo_city,
+    devices.geo_lat_long as last_geo_lat_long,
+    devices.event_time as last_event_time,
+    devices.event_id as last_event_id
 from devices
 left join identifies
-  on devices.id = identifies.device_id
+    on devices.id = identifies.device_id
 where
-  devices.id is not null
-  and identifies.device_id is not null
-  and devices.event_seq_num_desc = 1
-  and devices.source_type != 'server' -- exclude server events, they won't have geo or device values.
-  
+    devices.id is not null
+    and identifies.device_id is not null
+    and devices.event_seq_num_desc = 1
+    and devices.source_type != 'server' -- exclude server events, they won't have geo or device values.
--- models/events.sql
+++ models/events.sql
@@ -4,13 +4,17 @@
   )
 }}
 
-with events as (
+with staging as (
 
     select
-         {{ dbt_utils.star(ref('stg_fullstory__events'), except=["full_session_id_rn", "device_id_rn", "user_id_rn", "latest_user_id"]) }} 
+         {{ dbt_utils.star(ref('stg_fullstory__events'), except=["full_session_id_rn", "device_id_rn", "user_id_rn", "latest_user_id"]) }},
+
+         row_number() over(partition by event_id order by updated_time desc, processed_time desc) as event_id_rn
     from {{ ref("stg_fullstory__events") }}
+    where event_id_rn = 1
 
 )
+
 select
-    *
-from events
+  {{ dbt_utils.star(ref('stg_fullstory__events'), except=["full_session_id_rn", "device_id_rn", "user_id_rn", "latest_user_id"]) }}
+from staging
