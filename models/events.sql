14fba60a79598575cecc16b79ed0fea2c4e5f621
Robele Baker <robele@fullstory.com>
2025-07-23 19:56:19 +0200

    DECO-584 - Add incremental loading for events and improve README (#44)
    ## [0.9.3] - 2025-07-23
    - **Add incremental model support to events** - The `events.sql` model
    now supports incremental loading. Please check the `README.md` file for
    details on how to enable incremental loading.
    
    ---
    
    This pull request introduces version `0.9.3` of the `dbt_fullstory`
    package, adding incremental model support to the `events.sql` model and
    improving documentation for customizing model materialization. Key
    changes include updates to the `CHANGELOG.md`, `README.md`,
    `dbt_project.yml`, and the `events.sql` model to reflect these
    enhancements.
    
    ### New Features and Enhancements:
    
    * **Incremental Model Support**:
    - Added incremental loading support to the `events.sql` model, allowing
    it to process only new or updated records since the last run. This
    improves performance and reduces query costs. (`models/events.sql`,
    [models/events.sqlL15-R25](diffhunk://#diff-b465be9f35be60170b2c9c66f0b7472d82cba8d9c6e3856ed45b395fa569884aL15-R25))
    - Documented incremental modeling and its benefits in the `README.md`.
    (`README.md`,
    [README.mdL168-R202](diffhunk://#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5L168-R202))
    
    * **Customizing Model Materialization**:
    - Added documentation on how to configure models to materialize as a
    table, including BigQuery-specific optimizations. (`README.md`,
    [README.mdL168-R202](diffhunk://#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5L168-R202))
    
    ### Version Updates:
    
    * **Version Bump**:
    - Updated the package version to `0.9.3` in `dbt_project.yml` and
    `README.md`. (`dbt_project.yml`,
    [[1]](diffhunk://#diff-27386df54b2629c1191d8342d3725ed8678413cfa13b5556f59d69d33fae5425L2-R2);
    `README.md`,
    [[2]](diffhunk://#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5L158-R158)
    
    ### Documentation Updates:
    
    * **Changelog**:
    - Updated `CHANGELOG.md` to reflect the new features in version `0.9.3`.
    (`CHANGELOG.md`,
    [CHANGELOG.mdR8-R10](diffhunk://#diff-06572a96a58dc510037d5efa622f9bec8519bc1beab13c9f251e97e657a9d4edR8-R10))

--- models/events.sql
+++ models/events.sql
@@ -12,10 +12,15 @@ with staging as (
          row_number() over(partition by event_id order by updated_time desc, processed_time desc) as event_id_rn
     from {{ ref("stg_fullstory__events") }}
 
-
 )
 
 select
   {{ dbt_utils.star(ref('stg_fullstory__events'), except=["full_session_id_rn", "device_id_rn", "user_id_rn", "latest_user_id"]) }}
 from staging
+{% if is_incremental() %}
+and
+staging.updated_time >=  (select max(staging.updated_time) from {{ this }})  
+and
+staging.event_time >= {{ dbt.dateadd("hour", incremental_adjustment, dbt.current_timestamp()) }} 
+{% endif %}
 where event_id_rn = 1
