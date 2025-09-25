c62260c24205f48ecdc800323974aed9422d0b45
Robele Baker <robele@fullstory.com>
2025-09-25 15:44:59 +0200

    #45 - macro call in wrong spot (#46)
    This pull request makes a small but important change to the filtering
    logic in the `models/events.sql` file. The placement of the `where
    event_id_rn = 1` clause has been adjusted to ensure correct query
    execution and incremental logic.
    
    - SQL logic update:
    * Moved the `where event_id_rn = 1` clause to appear before the
    incremental filters, ensuring that the row number filter is always
    applied regardless of whether the query is running incrementally or as a
    full refresh.

--- models/events.sql
+++ models/events.sql
@@ -17,10 +17,10 @@ with staging as (
 select
   {{ dbt_utils.star(ref('stg_fullstory__events'), except=["full_session_id_rn", "device_id_rn", "user_id_rn", "latest_user_id"]) }}
 from staging
+where event_id_rn = 1
 {% if is_incremental() %}
 and
 staging.updated_time >=  (select max(staging.updated_time) from {{ this }})  
 and
 staging.event_time >= {{ dbt.dateadd("hour", incremental_adjustment, dbt.current_timestamp()) }} 
 {% endif %}
-where event_id_rn = 1
